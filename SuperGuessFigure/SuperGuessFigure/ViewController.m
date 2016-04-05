8d1d3c2a331c316fe4dbf280f9fe6ed1aabb2002
susu <28025920@qq.com>
2016-04-05 11:13:00 +0800

    实现了图片放大缩小动画

--- SuperGuessFigure/SuperGuessFigure/ViewController.m
+++ SuperGuessFigure/SuperGuessFigure/ViewController.m
@@ -9,15 +9,41 @@
 #import "ViewController.h"
 
 @interface ViewController ()
-
+@property (strong, nonatomic) IBOutlet UIButton *iconButton;
+@property(strong,nonatomic)UIButton* button;
 @end
 
 @implementation ViewController
-
+-(UIButton *)button{
+    if (_button==nil) {
+        _button=[[UIButton alloc]initWithFrame:CGRectMake(0, 0, self.view.bounds.size.width, self.view.bounds.size.height)];
+        _button.backgroundColor=[UIColor colorWithWhite:0.0 alpha:0.5];
+        _button.alpha=0.0;
+        [_button addTarget:self action:@selector(goBack:) forControlEvents:UIControlEventTouchUpInside];
+        [self.view addSubview:_button];
+    }
+    return _button;
+}
+-(UIStatusBarStyle)preferredStatusBarStyle{
+    return UIStatusBarStyleLightContent;
+}
 - (void)viewDidLoad {
     [super viewDidLoad];
     
 }
 
-
+- (IBAction)Enlarge:(UIButton *)sender {
+    [self button];
+    [self.view bringSubviewToFront:self.iconButton];
+    [UIView animateWithDuration:1.0f animations:^{
+        self.button.alpha=1.0;
+        self.iconButton.frame=CGRectMake(0, (self.view.bounds.size.height-self.view.bounds.size.width)/2, self.view.bounds.size.width, self.view.bounds.size.width);
+    }];
+}
+-(void)goBack:(UIButton*)button{
+    [UIView animateWithDuration:1.0f animations:^{
+        button.alpha=0.0;
+        self.iconButton.frame=CGRectMake(84, 122, 152, 144);
+    }];
+}
 @end
